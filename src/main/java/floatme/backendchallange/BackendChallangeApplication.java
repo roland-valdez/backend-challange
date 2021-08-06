93b9104d926a5adf0f9edd593dd35dfbb36c14c3
Roland Valdez <valdez.roland.23@gmail.com>
2021-08-05 23:56:44 -0500

    creating template for challenge

---
+++ src/main/java/floatme/backendchallange/BackendChallangeApplication.java
@@ -0,0 +1,13 @@
+package floatme.backendchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/floatme/backendchallange/BackendChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package floatme.backendchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

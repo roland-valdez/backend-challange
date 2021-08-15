7e80ebb8076da5f26d35864f50f1edde67ae93c3
Roland Valdez <valdez.roland.23@gmail.com>
2021-08-14 23:22:49 -0500

    added index controller

--- src/main/java/floatme/backendchallenge/controller/IndexController.java
+++ src/main/java/floatme/backendchallenge/controller/IndexController.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Index {
+public class IndexController {
 
         @GetMapping("/")
         public String index(){
 
-        return "index";
+        return "indexController";
         }
 }
